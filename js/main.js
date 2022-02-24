32844f04d54aa3561a817d37d4f9e3ad4b06d246
JimCarter44 <gontard.j@hotmail.fr>
2022-02-24 09:41:33 -0500

    animation page formation

---
+++ js/main.js
@@ -0,0 +1,323 @@
+jQuery(document).ready(function () {
+  let initiation = $("#initiation");
+  let windows = $("#windows");
+  let configuration = $("#configuration");
+  let dossiers = $("#dossiers");
+  let fichiers = $("#fichiers");
+  let securite = $("#securite");
+  let internet = $("#internet");
+  let recherche = $("#recherche");
+  let telechargement = $("#telechargement");
+  let messagerie = $("#messagerie");
+  let initiationP = $("#initiationP");
+  let windowsP = $("#windowsP");
+  let configurationP = $("#configurationP");
+  let dossiersP = $("#dossiersP");
+  let fichiersP = $("#fichiersP");
+  let securiteP = $("#securiteP");
+  let internetP = $("#internetP");
+  let rechercheP = $("#rechercheP");
+  let telechargementP = $("#telechargementP");
+  let messagerieP = $("#messagerieP");
+
+  initiation.click(function () {
+    initiationP.slideToggle("slow");
+    if (windowsP.css("display") === "block") {
+      windowsP.slideToggle("slow");
+    }
+    else if (configurationP.css("display") === "block") {
+      configurationP.slideToggle("slow");
+    }
+    else if (dossiersP.css("display") === "block") {
+      dossiersP.slideToggle("slow");
+    }
+    else if (fichiersP.css("display") === "block") {
+      fichiersP.slideToggle("slow");
+    }
+    else if (securiteP.css("display") === "block") {
+      securiteP.slideToggle("slow");
+    }
+    else if (internetP.css("display") === "block") {
+      internetP.slideToggle("slow");
+    }
+    else if (rechercheP.css("display") === "block") {
+      rechercheP.slideToggle("slow");
+    }
+    else if (telechargementP.css("display") === "block") {
+      telechargementP.slideToggle("slow");
+    }
+    else if (messagerieP.css("display") === "block") {
+      messagerieP.slideToggle("slow");
+    }
+  });
+  windows.click(function () {
+    windowsP.slideToggle("slow");
+    if (initiationP.css("display") === "block") {
+      initiationP.slideToggle("slow");
+    }
+    else if (configurationP.css("display") === "block") {
+      configurationP.slideToggle("slow");
+    }
+    else if (dossiersP.css("display") === "block") {
+      dossiersP.slideToggle("slow");
+    }
+    else if (fichiersP.css("display") === "block") {
+      fichiersP.slideToggle("slow");
+    }
+    else if (securiteP.css("display") === "block") {
+      securiteP.slideToggle("slow");
+    }
+    else if (internetP.css("display") === "block") {
+      internetP.slideToggle("slow");
+    }
+    else if (rechercheP.css("display") === "block") {
+      rechercheP.slideToggle("slow");
+    }
+    else if (telechargementP.css("display") === "block") {
+      telechargementP.slideToggle("slow");
+    }
+    else if (messagerieP.css("display") === "block") {
+      messagerieP.slideToggle("slow");
+    }
+  });
+  configuration.click(function () {
+    configurationP.slideToggle("slow");
+    if (windowsP.css("display") === "block") {
+      windowsP.slideToggle("slow");
+    }
+    else if (initiationP.css("display") === "block") {
+      initiationP.slideToggle("slow");
+    }
+    else if (dossiersP.css("display") === "block") {
+      dossiersP.slideToggle("slow");
+    }
+    else if (fichiersP.css("display") === "block") {
+      fichiersP.slideToggle("slow");
+    }
+    else if (securiteP.css("display") === "block") {
+      securiteP.slideToggle("slow");
+    }
+    else if (internetP.css("display") === "block") {
+      internetP.slideToggle("slow");
+    }
+    else if (rechercheP.css("display") === "block") {
+      rechercheP.slideToggle("slow");
+    }
+    else if (telechargementP.css("display") === "block") {
+      telechargementP.slideToggle("slow");
+    }
+    else if (messagerieP.css("display") === "block") {
+      messagerieP.slideToggle("slow");
+    }
+  });
+  dossiers.click(function () {
+    dossiersP.slideToggle("slow");
+    if (windowsP.css("display") === "block") {
+      windowsP.slideToggle("slow");
+    }
+    else if (configurationP.css("display") === "block") {
+      configurationP.slideToggle("slow");
+    }
+    else if (initiationP.css("display") === "block") {
+      initiationP.slideToggle("slow");
+    }
+    else if (fichiersP.css("display") === "block") {
+      fichiersP.slideToggle("slow");
+    }
+    else if (securiteP.css("display") === "block") {
+      securiteP.slideToggle("slow");
+    }
+    else if (internetP.css("display") === "block") {
+      internetP.slideToggle("slow");
+    }
+    else if (rechercheP.css("display") === "block") {
+      rechercheP.slideToggle("slow");
+    }
+    else if (telechargementP.css("display") === "block") {
+      telechargementP.slideToggle("slow");
+    }
+    else if (messagerieP.css("display") === "block") {
+      messagerieP.slideToggle("slow");
+    }
+  });
+  fichiers.click(function () {
+    fichiersP.slideToggle("slow");
+    if (windowsP.css("display") === "block") {
+      windowsP.slideToggle("slow");
+    }
+    else if (configurationP.css("display") === "block") {
+      configurationP.slideToggle("slow");
+    }
+    else if (dossiersP.css("display") === "block") {
+      dossiersP.slideToggle("slow");
+    }
+    else if (initiationP.css("display") === "block") {
+      initiationP.slideToggle("slow");
+    }
+    else if (securiteP.css("display") === "block") {
+      securiteP.slideToggle("slow");
+    }
+    else if (internetP.css("display") === "block") {
+      internetP.slideToggle("slow");
+    }
+    else if (rechercheP.css("display") === "block") {
+      rechercheP.slideToggle("slow");
+    }
+    else if (telechargementP.css("display") === "block") {
+      telechargementP.slideToggle("slow");
+    }
+    else if (messagerieP.css("display") === "block") {
+      messagerieP.slideToggle("slow");
+    }
+  });
+  securite.click(function () {
+    securiteP.slideToggle("slow");
+    if (windowsP.css("display") === "block") {
+      windowsP.slideToggle("slow");
+    }
+    else if (configurationP.css("display") === "block") {
+      configurationP.slideToggle("slow");
+    }
+    else if (dossiersP.css("display") === "block") {
+      dossiersP.slideToggle("slow");
+    }
+    else if (fichiersP.css("display") === "block") {
+      fichiersP.slideToggle("slow");
+    }
+    else if (initiationP.css("display") === "block") {
+      initiationP.slideToggle("slow");
+    }
+    else if (internetP.css("display") === "block") {
+      internetP.slideToggle("slow");
+    }
+    else if (rechercheP.css("display") === "block") {
+      rechercheP.slideToggle("slow");
+    }
+    else if (telechargementP.css("display") === "block") {
+      telechargementP.slideToggle("slow");
+    }
+    else if (messagerieP.css("display") === "block") {
+      messagerieP.slideToggle("slow");
+    }
+  });
+  internet.click(function () {
+    internetP.slideToggle("slow");
+    if (windowsP.css("display") === "block") {
+      windowsP.slideToggle("slow");
+    }
+    else if (configurationP.css("display") === "block") {
+      configurationP.slideToggle("slow");
+    }
+    else if (dossiersP.css("display") === "block") {
+      dossiersP.slideToggle("slow");
+    }
+    else if (fichiersP.css("display") === "block") {
+      fichiersP.slideToggle("slow");
+    }
+    else if (securiteP.css("display") === "block") {
+      securiteP.slideToggle("slow");
+    }
+    else if (initiationP.css("display") === "block") {
+      initiationP.slideToggle("slow");
+    }
+    else if (rechercheP.css("display") === "block") {
+      rechercheP.slideToggle("slow");
+    }
+    else if (telechargementP.css("display") === "block") {
+      telechargementP.slideToggle("slow");
+    }
+    else if (messagerieP.css("display") === "block") {
+      messagerieP.slideToggle("slow");
+    }
+  });
+  recherche.click(function () {
+    rechercheP.slideToggle("slow");
+    if (windowsP.css("display") === "block") {
+      windowsP.slideToggle("slow");
+    }
+    else if (configurationP.css("display") === "block") {
+      configurationP.slideToggle("slow");
+    }
+    else if (dossiersP.css("display") === "block") {
+      dossiersP.slideToggle("slow");
+    }
+    else if (fichiersP.css("display") === "block") {
+      fichiersP.slideToggle("slow");
+    }
+    else if (securiteP.css("display") === "block") {
+      securiteP.slideToggle("slow");
+    }
+    else if (internetP.css("display") === "block") {
+      internetP.slideToggle("slow");
+    }
+    else if (initiationP.css("display") === "block") {
+      initiationP.slideToggle("slow");
+    }
+    else if (telechargementP.css("display") === "block") {
+      telechargementP.slideToggle("slow");
+    }
+    else if (messagerieP.css("display") === "block") {
+      messagerieP.slideToggle("slow");
+    }
+  });
+  telechargement.click(function () {
+    telechargementP.slideToggle("slow");
+    if (windowsP.css("display") === "block") {
+      windowsP.slideToggle("slow");
+    }
+    else if (configurationP.css("display") === "block") {
+      configurationP.slideToggle("slow");
+    }
+    else if (dossiersP.css("display") === "block") {
+      dossiersP.slideToggle("slow");
+    }
+    else if (fichiersP.css("display") === "block") {
+      fichiersP.slideToggle("slow");
+    }
+    else if (securiteP.css("display") === "block") {
+      securiteP.slideToggle("slow");
+    }
+    else if (internetP.css("display") === "block") {
+      internetP.slideToggle("slow");
+    }
+    else if (rechercheP.css("display") === "block") {
+      rechercheP.slideToggle("slow");
+    }
+    else if (initiationP.css("display") === "block") {
+      initiationP.slideToggle("slow");
+    }
+    else if (messagerieP.css("display") === "block") {
+      messagerieP.slideToggle("slow");
+    }
+  });
+  messagerie.click(function () {
+    messagerieP.slideToggle("slow");
+    if (windowsP.css("display") === "block") {
+      windowsP.slideToggle("slow");
+    }
+    else if (configurationP.css("display") === "block") {
+      configurationP.slideToggle("slow");
+    }
+    else if (dossiersP.css("display") === "block") {
+      dossiersP.slideToggle("slow");
+    }
+    else if (fichiersP.css("display") === "block") {
+      fichiersP.slideToggle("slow");
+    }
+    else if (securiteP.css("display") === "block") {
+      securiteP.slideToggle("slow");
+    }
+    else if (internetP.css("display") === "block") {
+      internetP.slideToggle("slow");
+    }
+    else if (rechercheP.css("display") === "block") {
+      rechercheP.slideToggle("slow");
+    }
+    else if (telechargementP.css("display") === "block") {
+      telechargementP.slideToggle("slow");
+    }
+    else if (initiationP.css("display") === "block") {
+      initiationP.slideToggle("slow");
+    }
+  });
+})
